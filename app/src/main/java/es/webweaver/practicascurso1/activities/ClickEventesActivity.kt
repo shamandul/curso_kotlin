28802e284499afcf9a3fd696a3f4a6a21dc9f6ac
Jesús S.P <shamandul@gmail.com>
2018-10-23 21:11:20 +0200

    Curso Kotlin: Life Click Eventes Activity

--- app/src/main/java/es/webweaver/practicascurso1/activities/ClickEventesActivity.kt
+++ app/src/main/java/es/webweaver/practicascurso1/activities/ClickEventesActivity.kt
@@ -2,12 +2,51 @@ package es.webweaver.practicascurso1.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import es.webweaver.practicascurso1.R
 
-class ClickEventesActivity : AppCompatActivity() {
+class ClickEventesActivity : AppCompatActivity(), View.OnLongClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_click_eventes)
+
+        clickInLine()
+
+        val btn1 = findViewById<Button>(R.id.buttonClickMulti)
+        val btn2 = findViewById<Button>(R.id.buttonClickMulti1)
+        val btn3 = findViewById<Button>(R.id.buttonClickMulti2)
+
+        btn1.setOnLongClickListener(this)
+        btn2.setOnLongClickListener(this)
+        btn3.setOnLongClickListener(this)
     }
+
+    /*
+     * Método tiene que ser publico
+     * Los nombre tienen que coincidir
+     * Y Recibe un sólo parámetro (View)
+     */
+    fun xmlClick(view :View){
+        Toast.makeText(this, "Click By XML", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun clickInLine(){
+        val btn = findViewById<Button>(R.id.buttonClickInLine)
+        btn.setOnClickListener { Toast.makeText(this, "Click in Line", Toast.LENGTH_SHORT).show() }
+    }
+
+    override fun onLongClick(view: View): Boolean {
+       when(view.id){
+           R.id.buttonClickMulti -> Toast.makeText(this, "Click Multi 1", Toast.LENGTH_SHORT).show()
+           R.id.buttonClickMulti1 -> Toast.makeText(this, "Click Multi 2", Toast.LENGTH_SHORT).show()
+           R.id.buttonClickMulti2 -> Toast.makeText(this, "Click Multi 3", Toast.LENGTH_SHORT).show()
+       }
+        return true
+    }
+
+
 }
--- app/src/main/java/es/webweaver/practicascurso1/activities/LifeCycleActivity.kt
+++ app/src/main/java/es/webweaver/practicascurso1/activities/LifeCycleActivity.kt
@@ -2,12 +2,28 @@ package es.webweaver.practicascurso1.activities
 
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import es.webweaver.practicascurso1.R
 
 class LifeCycleActivity : LifeCycleEventsActivity() {
 
+    private var exitEnable = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
+    }
+
+    // boton atrás personalizado
+    override fun onBackPressed() {
+        //super.onBackPressed()
+        if (exitEnable){
+            super.onBackPressed()
+        }
+        exitEnable = true
+        Toast.makeText(this,"Pulsa atrás para salir de la ventana", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { exitEnable = false }, 2000)
     }
 }
