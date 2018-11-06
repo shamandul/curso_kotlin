80838ec02696317030677eb4a3e3c72a398b4f7c
Jesús S.P <shamandul@gmail.com>
2018-11-06 21:19:43 +0100

    Curso Kotlin: Extension Functions

--- app/src/main/java/es/webweaver/practicascurso1/activities/ExtensionFunctionsActivity.kt
+++ app/src/main/java/es/webweaver/practicascurso1/activities/ExtensionFunctionsActivity.kt
@@ -3,8 +3,10 @@ package es.webweaver.practicascurso1.activities
 
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import es.webweaver.practicascurso1.MainActivity
 import es.webweaver.practicascurso1.R
-import es.webweaver.practicascurso1.others.ToolbarActivity
+import es.webweaver.practicascurso1.others.*
+import kotlinx.android.synthetic.main.activity_extension_functions.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class ExtensionFunctionsActivity : ToolbarActivity() {
@@ -14,5 +16,10 @@ class ExtensionFunctionsActivity : ToolbarActivity() {
         setContentView(R.layout.activity_extension_functions)
 
         toolbarToLoad(toolbar as Toolbar)
+
+        buttonToast.setOnClickListener { toast("hola") }
+        buttonSnackBar.setOnClickListener { snackBar("HOLA", action = "Undo"){toast("undo")}  }
+        buttonLoadByUrl.setOnClickListener { imageViewLoadedByUrl.loadByURL("https://static.pexels.com/photos/288264/pexels-photo-288264.jpeg") }
+        buttonGoToActivity.setOnClickListener { goToActivity<MainActivity>() }
     }
 }
--- app/src/main/java/es/webweaver/practicascurso1/adapters/PersonAdapter.kt
+++ app/src/main/java/es/webweaver/practicascurso1/adapters/PersonAdapter.kt
@@ -1,17 +1,17 @@
 package es.webweaver.practicascurso1.adapters
 
 import android.content.Context
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 import es.webweaver.practicascurso1.model.Person
+import es.webweaver.practicascurso1.others.inflate
 import kotlinx.android.synthetic.main.list_view_person.view.*
 
 class PersonAdapter(val context: Context,val layout:Int, val list: List<Person>): BaseAdapter(){
 
-    private val mInflator: LayoutInflater = LayoutInflater.from(context)
+    //private val mInflator: LayoutInflater = LayoutInflater.from(context)
 
     override fun getItem(position: Int): Any {
        return list[position]
@@ -25,13 +25,14 @@ class PersonAdapter(val context: Context,val layout:Int, val list: List<Person>)
         return list.size
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         val view: View
         val vh: PersonViewHolder
 
         if (convertView==null){
-            view = mInflator.inflate(layout,parent,false)
+          //  view = mInflator.inflate(layout,parent,false)
+            view = parent.inflate(layout)
             vh = PersonViewHolder(view)
             view.tag = vh
         }else{
